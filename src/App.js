9e9d0bb896379898fab829b5611df34ff5f64eae
Vanderllee <vanderllee@gmail.com>
2021-09-22 15:25:59 -0300

    added router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,59 @@
-/** import Home from "./screens/home/Home"; */
-/** import Single from "./screens/single/Single"; */
-/*import Write from "./screens/write/Write";*/
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-import TopBar from "./components/topbar/TopBar";
-import Settings from "./screens/settings/Settings";
+import Login from "./screens/login/Login"
+import Register from "./screens/register/Register"
+import Home from "./screens/home/Home"
+import Single from "./screens/single/Single"
+import Write from "./screens/write/Write"
+import Settings from "./screens/settings/Settings"
+import TopBar from "./components/topbar/TopBar"
+
+
 
 function App() {
   return (
-    <>
+    <Router>
       <TopBar />
-      <Settings />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
 
-      {/** <Write /> */} {/** to writting ours posts */}
-      {/** <Single /> */} {/** to show our page content */}
-      {/** <Home /> */} {/** home page is to show everything we have on page */}
-    </>
+          <Route path="/cadastro">
+            <Register />
+          </Route>
+
+          <Route path="/write">
+            <Write /> 
+          </Route>
+
+          <Route path="/login">
+            <Login />
+          </Route>
+
+          <Route path="/settings">
+            <Settings />
+          </Route>
+
+          <Route path="/post/:postId">
+            <Single /> 
+          </Route>
+
+        </Switch>
+
+        
+
+     
+      {/**  */} {/** logar screen */}
+      {/**  */} {/** config our posts */}
+      {/** */} {/** to show our page content */}
+     
+    </Router>
   );
 }
 
