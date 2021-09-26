fe57fe8a1febf594409cbed80eaad507c3e48408
Vanderllee <vanderllee@gmail.com>
2021-09-26 10:31:42 -0300

    added users

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const Post = require('../models/Post')
 const bcrypt = require('bcrypt')
 
 
@@ -41,6 +42,52 @@ router.put('/:id', async (req, resp) => {
     
 })
 
+
+//Delete
+
+router.delete('/:id', async (req, resp) => {
+    
+    if(req.body.userId === req.params.id) {
+
+        try {
+
+            const user = await User.findById(req.params.id)
+            
+            try {
+                await Post.deleteMany({username: user.username})
+                await User.findByIdAndDelete(req.params.id)
+
+                resp.status(200).json('Usuario deletado com sucesso!')
+
+            } catch(error) {
+                resp.status(500).json(error)
+            }
+        
+
+        } catch (error) {
+            resp.status(404).json("Usuário não encontrado!")
+        }
+
+    } else {
+        resp.status(401).json("usuário não encontrado!")
+    }
+    
+})
+
+//get user
+router.get('/:id', async (req, res) => {
+
+    try {
+        const user = await User.findById(req.params.id)
+
+        const { password, ...others } = user._doc
+
+        res.status(200).json(others)
+
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
 
 
 module.exports = router
