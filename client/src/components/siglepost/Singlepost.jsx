97c29a3cf574c608171e5c288db3e09a94088f65
Vanderllee <vanderllee@gmail.com>
2021-09-29 17:29:51 -0300

    Updated single post

--- client/src/components/siglepost/Singlepost.jsx
+++ client/src/components/siglepost/Singlepost.jsx
@@ -1,28 +1,60 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import { useLocation } from 'react-router'
 import './singlepost.css'
 import axios from 'axios'
 import { Link } from 'react-router-dom';
+import { Context } from '../../context/Context';
+
+
+
 
 export default function Singlepost() {
 
     const location = useLocation();
     const path = location.pathname.split('/')[2]
     const [post, setPost] = useState({})
+    const { user }=useContext( Context )
+    const [ title, setTitle ] = useState('')
+    const [ desc, setDesc ] = useState('')
+    const [ updateMode, setUpdateMode ] = useState(false)
 
     const PF="http://localhost:5000/images/"
  
-
 
     useEffect(() => {
         const getPost = async () => {
             const response = await axios.get(`/posts/${path}`)
 
             setPost(response.data)
+            setTitle(response.data.title)
+            setDesc(response.data.desc)
         } 
 
         getPost();
     }, [path])
+
+    const handleDelete = async () => {
+        try {
+            await axios.delete(`/posts/${post._id}`, {data: { username: user.username }})
+
+            window.location.replace('/')
+
+        } catch (err) {
+            //
+        }
+    }
+
+   const handleUpdate = async () => {
+        try {
+            await axios.put(`/posts/${post._id}`, { username: user.username, title, desc })
+
+            //window.location.reload()
+            setUpdateMode( false )
+        } catch (err) {
+         //
+        }
+   }
+
 
     return (
         <div className="singlepost">
@@ -36,14 +68,36 @@ export default function Singlepost() {
 
                 )}
 
-                <h1 className="singlePostTitle">
-                    { post.title }
+                { updateMode 
+                    ? (<input 
+                        type="text" 
+                        value={ title }  
+                        className="singlePostTitleInput"
+                        autoFocus
+                        onChange={(e) => setTitle(e.target.value)}
+                    />)
+                    : (<h1 className="singlePostTitle">
+                        { title }
 
-                    <div className="singlePostEdit">
-                        <i className=" singlePostIcon far fa-edit"></i>
-                        <i className=" singlePostIcon far fa-trash-alt"></i>
-                    </div>  
-                </h1>
+                        {  post.username === user?.username && 
+                        (<div className="singlePostEdit">
+
+                                <i className=" singlePostIcon far fa-edit" 
+                                    onClick={() => { setUpdateMode(true) }}
+                                ></i>
+                                <i className=" singlePostIcon far fa-trash-alt" 
+                                    onClick={ handleDelete }
+                                ></i>
+                        
+                            </div>
+                        )
+                            
+                        } 
+                        </h1>
+                    )
+                
+                }
+
 
                 <div className="singlePostInfo">
                     <span className="singlePostAuthor">
@@ -58,11 +112,19 @@ export default function Singlepost() {
                     </span>
                 </div>
 
-                <p className="singlePostDesc">
-                    
-                 { post.desc }
+                {
+                    updateMode 
+                    ? (<textarea 
+                        className="singlePostDescInput" 
+                        value={ desc }
+                        onChange={(e) => setDesc(e.target.value)} 
+                    />)
+                    : (<p className="singlePostDesc"> { desc } </p>)
+                }
 
-                </p>
+               { updateMode 
+                && (<button className="singlePostButton"onClick={ handleUpdate }>Atualizar</button>)
+                }
             </div>
         </div>
     )
